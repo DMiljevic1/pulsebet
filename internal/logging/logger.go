011762fafa912366307075f50e5110953cb1a2fb
unknown <duje.miljevic@gmail.com>
2025-11-19 20:07:17 +0100

    created logger

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -1,1 +1,15 @@
 package logging
+
+import (
+	"log/slog"
+	"os"
+)
+
+func New(serviceName string) *slog.Logger {
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+
+	logger := slog.New(handler).With("service", serviceName)
+	return logger
+}
